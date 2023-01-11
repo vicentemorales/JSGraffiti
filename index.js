cca91d46ad1fc173bbe7adf0b933b6b3089121aa
vicentemorales <vmorales121@gmail.com>
2023-01-10 19:34:25 -0800

    Finished the three bullet points of the assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+for(let x = 0; x < dogImages.length; x++){   
+    dogImages[x].style.transform = 'rotate(180deg)'
+}
+}
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+	dogNames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'red'
+
+
 
 // Select some elements...
